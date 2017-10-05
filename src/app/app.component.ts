08f79601d11b0ae9cbf2be043b1bc38e4dff6e7f
Sávio Bispo Maciel <savio.maciel@basis.com.br>
2017-10-05 09:25:45 -0300

    Criando componente hero-detail.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,14 +20,6 @@ const HEROES: Hero[] = [
   styleUrls: ['./app.component.css'],
   template: `
               <h1>{{title}}</h1>       
-              <div *ngIf="selectedHero">   
-                  <h2>{{selectedHero.name}} details!</h2>
-                  <div><label>id: </label>{{selectedHero.id}}</div>
-                  <div>
-                      <label>name: </label>
-                      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-                  </div>
-              </div>
               <h2>My Heroes</h2>
               <ul class="heroes">             
                 <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
